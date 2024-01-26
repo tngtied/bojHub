44e9703cbd8929383eb06e99107b74c7fb5497a3
tngtied <75424805+tngtied@users.noreply.github.com>
2024-01-26 17:21:42 +0900

    [Gold V] Title: 합분해, Time: 1280 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,19 +1,17 @@
-import sys
-from math import comb
+import sys 
 input = sys.stdin.readline
 print = sys.stdout.write
 
 n, k = map(int, input().split())
-dp = [[0 for _ in range(k + 1)] for __ in range(n + 1)]
-for i in range(0, n + 1):
-    dp[i][1] = 1
 
-for l in range(k):
-    # 사용한 수 개수
-    for i in range(n + 1):
-        for j in range(i + 1):
-            # 이전 dp
-                dp[i][l + 1] += dp[i - j][l]
-                dp[i][l + 1] %= 1000000000
-print("%d" % dp[n][k])
-        
+dp = [[0 for __ in range(k)] for _ in range(n + 1)]
+for i in range(n + 1):
+    dp[i][0] += 1
+    
+for i in range(n + 1):
+    for j in range(1, k):
+        for l in range(i + 1):
+            dp[i][j] += dp[i - l][j - 1]
+            dp[i][j] %= 1000000000
+
+print("%d" % dp[n][k - 1])
