555dad1a4f4869fe6560e10e27b95c5f3a0f201c
tngtied <75424805+tngtied@users.noreply.github.com>
2024-01-28 13:49:22 +0900

    [Gold IV] Title: N-Queen, Time: 8032 ms, Memory: 127720 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,31 +1,33 @@
 import sys
 input = sys.stdin.readline
 print = sys.stdout.write
+n = int(input())
 
-def n_queen(count, pos):
+vertical = [False] * n
+left = [False] * (2 * n - 1)
+## x + y
+right = [False] * (2 * n - 1)
+## x - y + n - 1
+
+def solution(pos, count):
     if (pos // n == n - 1):
-        if (count == n):
+        if count == n:
             return 1
         else:
             return 0
-        
     result = 0
     for i in range(n):
         npos = (pos // n + 1) * n + i
-        nx, ny = npos // n, npos % n
-        left = nx + ny
-        right = nx - ny + n - 1
-        if (y[ny] or l[left] or r[right]):
+        x = npos // n 
+        y = npos % n
+        l = x + y
+        r = x - y + n - 1
+        if (vertical[y] or left[l] or right[r]):
             continue
-        x[nx] = y[ny] = l[left] = r[right] = True
-        result += n_queen(count + 1, npos)
-        x[nx] = y[ny] = l[left] = r[right] = False
-    return result
-            
 
-n = int(input())
-x = [False] * (n)
-y = [False] * n
-l = [False] * 2 * n
-r = [False] * 2 * n
-print("%d" % (n_queen(0, -1)))
+        vertical[y] = left[l] = right[r] = True
+        result += solution(npos, count + 1)
+        vertical[y] = left[l] = right[r] = False
+    return result
+
+print("%d" % solution(-1, 0))
