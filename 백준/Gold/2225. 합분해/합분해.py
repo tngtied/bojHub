a55fbfe5e1780979366d12ff6241eaff8b151e27
tngtied <75424805+tngtied@users.noreply.github.com>
2024-02-01 15:48:19 +0900

    [Gold V] Title: 합분해, Time: 1252 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,17 +1,14 @@
-import sys 
+import sys
 input = sys.stdin.readline
 print = sys.stdout.write
 
-n, k = map(int, input().split())
-
-dp = [[0 for __ in range(k)] for _ in range(n + 1)]
+n, k = map(int, input().strip().split())
+sumlist = [[0 for __ in range(k)] for _ in range(n + 1)]
 for i in range(n + 1):
-    dp[i][0] += 1
-    
+    sumlist[i][0] = 1
 for i in range(n + 1):
-    for j in range(1, k):
-        for l in range(i + 1):
-            dp[i][j] += dp[i - l][j - 1]
-            dp[i][j] %= 1000000000
-
-print("%d" % dp[n][k - 1])
+    for l in range(1, k):
+        for j in range(i + 1):
+            sumlist[i][l] += sumlist[i - j][l - 1]
+            sumlist[i][l] %= 1000000000
+print("%d" % sumlist[n][k - 1])
