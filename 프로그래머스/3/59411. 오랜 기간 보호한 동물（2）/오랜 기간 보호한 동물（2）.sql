0d6b093607348be60296be47c2f118a7b1a846d7
tngtied <75424805+tngtied@users.noreply.github.com>
2024-02-20 00:34:57 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
 SELECT i.animal_id, i.name
 from ANIMAL_INS as i inner join animal_outs as o on i.animal_id = o.animal_id
-order by datediff(o.datetime, i.datetime) desc
+order by timestampdiff(day, i.datetime, o.datetime) desc
 limit 2;
