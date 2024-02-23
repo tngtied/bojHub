66b88512ba999772a0aef09f38826ca3c699d9b3
tngtied <75424805+tngtied@users.noreply.github.com>
2024-02-23 10:34:32 +0900

    [Silver I] Title: 격자상의 경로, Time: 44 ms, Memory: 33240 KB, Score: 32 point -BaekjoonHub

---
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
@@ -0,0 +1,26 @@
+import sys, math
+input = sys.stdin.readline
+print = sys.stdout.write
+n, m, k = map(int, input().split())
+if k == 0:
+    kx, ky = 0, 0
+elif (k % m == 0):
+    kx = k // m - 1
+    ky = m
+else:
+    kx, ky = k // m, k % m - 1
+dp = [[0 for __ in range(m)] for _ in range(n)]
+# 세로, 가로
+dp[0][0] = 1
+dxy = [(0, -1), (-1, 0)]
+def solution(sx, sy, ex, ey):
+    for j in range(sy, ey + 1):
+        for i in range(sx, ex + 1):
+            for d in dxy:
+                px, py = i + d[0], j + d[1]
+                if (px < sx or py < sy):
+                    continue
+                dp[i][j] += dp[px][py]
+solution(0, 0, kx, ky)
+solution(kx, ky, n - 1, m - 1)
+print ("%d" % dp[n - 1][m - 1])
