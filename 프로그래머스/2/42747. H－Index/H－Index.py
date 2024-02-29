c0fdad7e81040eacaa2a3572bfebe6073f4968ea
tngtied <75424805+tngtied@users.noreply.github.com>
2024-03-01 02:37:17 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -2,6 +2,7 @@ def solution(citations):
     citations.sort(reverse = True)
     n = len(citations)
     for i in range(n):
-        if (i + 1 > citations[i]):
+        print("citations[i] %d, i + 1 %d, n - 1 - i %d" % (citations[i] , i + 1, n - 1 - i))
+        if (i + 1 > citations[i] ):
             return i
     return n
