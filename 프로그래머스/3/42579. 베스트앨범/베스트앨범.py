42eea142ad73ea6367241ca13c483995d3726592
tngtied <75424805+tngtied@users.noreply.github.com>
2024-02-29 15:39:03 +0900

    [level 3] Title: 베스트앨범, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,23 +1,26 @@
 def solution(genres, plays):
     answer = []
-    hashmap = {}
-    total_dict = {}
+    play_map = {}
+    genre_map = {}
+    
     for i in range(len(genres)):
-        if (genres[i] in hashmap):
-            hashmap[genres[i]].append((plays[i], i))
-            total_dict[genres[i]] += plays[i]
+        if genres[i] in play_map:
+            play_map[genres[i]].append((i, plays[i]))
+            genre_map[genres[i]] += plays[i]
         else:
-            hashmap[genres[i]] = [(plays[i], i)]
-            total_dict[genres[i]] = plays[i]
-    sorted_genre = sorted(total_dict.items(), key = lambda x: total_dict[x[0]], reverse = True)
-    for gen in sorted_genre:
-        print(">>gen %s" % str(gen))
-        key_genre = gen[0]
-        hashmap[key_genre].sort(reverse = True)
-        if (len(hashmap[key_genre]) > 1 and hashmap[key_genre][0][0] == hashmap[key_genre][1][0]):
-            answer.append(min(hashmap[key_genre][0][1], hashmap[key_genre][1][1]))
-            answer.append(max(hashmap[key_genre][0][1], hashmap[key_genre][1][1]))
+            play_map[genres[i]] = [(i, plays[i])]
+            genre_map[genres[i]] = plays[i]
+    sorted_genre = sorted(genre_map.items(), key = lambda x : genre_map[x[0]], reverse = True)
+    print(str(sorted_genre))
+    for genre in sorted_genre:
+        play_map[genre[0]].sort(key = lambda x : x[1], reverse = True)
+        sorted_songs = play_map[genre[0]]
+        print(str(sorted_songs))
+        song_num = min(len(sorted_songs), 2)
+        if song_num == 2 and sorted_songs[0][1] == sorted_songs[1][1]:
+            answer.append(min(sorted_songs[0][0], sorted_songs[1][0]))
+            answer.append(max(sorted_songs[0][0], sorted_songs[1][0]))
         else:
-            for j in range(min(len(hashmap[key_genre]), 2)):
-                answer.append(hashmap[key_genre][j][1])    
+            for i in range(song_num):
+                answer.append(sorted_songs[i][0])
     return answer
